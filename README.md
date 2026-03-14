7ea7642bc23cb65b76ff50ffb7d63c680c905691
yumarie <yumarie@umich.edu>
2026-03-14 03:40:44 -0400

    add high level model

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 terminal.minesweeper
 ===
+
 Play minesweeper on the terminal
 
 A TUI built in Go using Charm libraries like bubbletea, lipgloss, and bubbles
--- main.go
+++ main.go
@@ -1,1 +1,17 @@
-package terminalminesweeper
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "charm.land/bubbletea/v2"
+	"terminal.minesweeper/tui"
+)
+
+func main() {
+	p := tea.NewProgram(tui.Model())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Oops, looks like the program hit a bomb: %v", err)
+		os.Exit(1)
+	}
+}
