4c04fce2f6afc4b8cae2a6d0ddd67004eb96978b
yumarie <yumarie@umich.edu>
2026-03-15 03:03:36 -0400

    update styling

--- tui/views/styles.go
+++ tui/views/styles.go
@@ -3,13 +3,15 @@ package views
 import lipgloss "charm.land/lipgloss/v2"
 
 var (
+	cursorColor = lipgloss.Color("#fd7658")
+
 	optionStyle = lipgloss.NewStyle().
 			Padding(0, 2)
 
 	selectedOptionStyle = lipgloss.NewStyle().
 				Padding(0, 2).
 				Foreground(lipgloss.Color("0")).
-				Background(lipgloss.Color("#FF5733")).
+				Background(cursorColor).
 				Bold(true)
 
 	iconStyle = lipgloss.NewStyle().
@@ -17,13 +19,13 @@ var (
 			Padding(0, 1)
 
 	listStyle  = lipgloss.NewStyle().Padding(1)
-	titleStyle = lipgloss.NewStyle().Padding(0, 1)
+	titleStyle = lipgloss.NewStyle().Padding(1, 1, 0)
 
 	tileStyle = lipgloss.NewStyle().Padding(0, 0).Width(2).Height(1).AlignHorizontal(lipgloss.Center).
 			AlignVertical(lipgloss.Center)
-	boardStyle = lipgloss.NewStyle().Padding(0, 0).Border(lipgloss.RoundedBorder())
+	boardStyle = lipgloss.NewStyle().Padding(0, 0).Border(lipgloss.NormalBorder())
 
-	cursor = tileStyle.Background(lipgloss.Color("#FF5733"))
+	cursor = tileStyle.Foreground(lipgloss.Color("0")).Background(cursorColor)
 )
 
 func Screen(width int, height int) lipgloss.Style {
