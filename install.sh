ea72493031deec2396dbf72977944d0fc2f072fd
yumarie <yumarie@umich.edu>
2026-03-20 16:47:05 -0400

    finalize first version of naive install script

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 
-REPO="yum25/terminal.minesweeper"
+APP_NAME="terminal.minesweeper"
+REPO="yum25/$APP_NAME"
 BINARY="tsweep"
 INSTALL_DIR="/usr/local/bin"
 
@@ -19,19 +20,22 @@ case $ARCH in
 esac
 
 TEMP_DIR=$(mktemp -d)
-TEMP_BINARY="${TEMP_DIR}/${BINARY}"
+TEMP_APP="${TEMP_DIR}/${APP_NAME}"
 
-URL="https://github.com/${REPO}/releases/download/${VERSION}/${BINARY}_${OS}_${ARCH}.tar.gz"
-echo "Downloading ${BINARY} ${VERSION} for ${OS}/${ARCH}..."
-curl -sL "$URL" -o "$TEMP_BINARY"
-tar xz -C "$TEMP_DIR" -f "${TEMP_BINARY}.tar.gz"
+cleanup() {
+    rm -rf "$TEMP_DIR"
+    echo "Cleaning up..."
+}
+trap cleanup EXIT
+
+URL="https://github.com/${REPO}/releases/download/${VERSION}/${APP_NAME}_${OS}_${ARCH}.tar.gz"
+echo "Downloading ${APP_NAME} ${VERSION} for ${OS}/${ARCH} from $URL..."
+curl -sL "$URL" -o "${TEMP_APP}.tar.gz"
+tar -xz -C "$TEMP_DIR" -f "${TEMP_APP}.tar.gz"
 
 
-echo "Installing to ${INSTALL_DIR}/${BINARY}..."
-mv "$TEMP_BINARY" "${INSTALL_DIR}/${BINARY}"
+echo "Installing ${BINARY} to ${INSTALL_DIR}..."
+mv "${TEMP_DIR}/${BINARY}" "${INSTALL_DIR}"
 chmod +x "${INSTALL_DIR}/${BINARY}"
-
-echo "Cleaning up..."
-rm -rf "$TEMP_DIR"
 
 echo "Time to start sweeping! Run '${BINARY}' to get started."
