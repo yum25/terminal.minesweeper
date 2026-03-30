ede6ab091802036e078ae7cc0b69becb7be8914a
yumarie <yumarie@umich.edu>
2026-03-29 23:07:01 -0400

    change condition for debug mode

--- main.go
+++ main.go
@@ -8,17 +8,14 @@ import (
 	"terminal.minesweeper/tui"
 )
 
-const dev = true
-
 func main() {
-	if dev {
+	if len(os.Getenv("DEBUG")) > 0 {
 		f, err := tea.LogToFile("debug.log", "debug")
 		if err != nil {
 			fmt.Println("fatal:", err)
 			os.Exit(1)
 		}
 		defer f.Close()
-
 	}
 
 	p := tea.NewProgram(tui.Model())
